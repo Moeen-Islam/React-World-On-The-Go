d9884d28a4780421807f1232b00699e0c63d8a23
Moeen-Islam <moeenislam809@gmail.com>
2026-03-28 16:07:29 +0600

    Count Function added

--- src/Component/Countries/Countries.jsx
+++ src/Component/Countries/Countries.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { use } from "react";
 import Country from '../Country/Country';
 import "./Countries.css"
 
 const Countries = ({countriesPromise}) => {
+
+    const [visitedCountries, setVisitedCountries] = useState([])
+    const handelVisitedCountries = (country) => {
+
+        const exists = visitedCountries.find(c => c.cca3 === country.cca3);
+
+            if (exists) {
+                const updated = visitedCountries.filter(
+                c => c.cca3 !== country.cca3
+                );
+                setVisitedCountries(updated);
+            } else {
+                const newVisitedCountries  = [...visitedCountries,country]
+                setVisitedCountries(newVisitedCountries)
+            }
+        
+    }   
     const countriesData = use(countriesPromise)
     const countries = countriesData.countries
     return (
-        <div className='countries'>
+        <div>
+            <h3>Total Country : {countries.length}</h3>
+            <h3>Total Country Visited : {visitedCountries.length}</h3>
+            <div className='countries'>
             {
-                countries.map(country => <Country key =  {country.cca3.cca3} country ={country}></Country>)
+                countries.map(country => <Country 
+                    key =  {country.cca3.cca3} 
+                    country ={country}
+                    handelVisitedCountries = {handelVisitedCountries}
+                    ></Country>)
             }
         </div>
+        </div>
+        
     );
 };
 
--- src/Component/Country/Country.jsx
+++ src/Component/Country/Country.jsx
@@ -1,24 +1,25 @@
 import "./Country.css"
 import React, { useState } from 'react';
 
-const Country = ({country}) => {
+const Country = ({country, handelVisitedCountries}) => {
 
     const [visited, setVisited] = useState(false)
     const handelVisited = () => {
         // visited? setVisited(false) : setVisited(true) 1st System
         // setVisited(visited? false:true) 2nd system
         setVisited(!visited)
+        handelVisitedCountries(country)
     }
 
 
     return (
-        <div className={`country`}>
+        <div className={`country ${visited && 'country-visited'}`}>
             <div className="image-box">
                 <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             </div>
             <h3>Name : {country.name.common} </h3>
             <p>Population : {country.population.population}</p>
-            <button onClick={handelVisited} className={`${visited && 'country-visited'}`}>
+            <button onClick={handelVisited}>
                 {
                     visited? "Visited" : "Not Visited"
                 }
